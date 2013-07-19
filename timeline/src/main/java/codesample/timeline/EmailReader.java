7ab1b9f3f4d6a676d74b760a48d6f67390c019d5
shyguy <suppusmac@gmail.com>
2013-07-19 16:16:52 -0400

    Add class

--- timeline/src/main/java/codesample/timeline/EmailReader.java
+++ timeline/src/main/java/codesample/timeline/EmailReader.java
@@ -11,7 +11,7 @@ import org.joda.time.DateTime;
 //A class that simply reads E-mails while EmailEvent is situational.
 public class EmailReader //I need to extend or implement something.
 {
-	Path start = Paths.get("C:\\Users\mcarlton\Desktop\allen-p._sent_mail.15Emails");
+	Path start = Paths.get("C:/Users/mcarlton/Desktop/allen-p._sent_mail.15Emails");
 	Files.walkFileTree(start, new SimpleFileVistor<path>() {
 		public FileVisitResult file(Path filePath, BasicFileAttributes attrs) throws IOException
 		{
