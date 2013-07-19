351be691c4eaa3602049d68d4466f0dd47fe9f18
NateC <nate.carpenter@gmail.com>
2013-07-18 20:02:50 -0400

    Fixes #1 - added javadoc to Timeline class

--- timeline/src/main/java/codesample/timeline/Timeline.java
+++ timeline/src/main/java/codesample/timeline/Timeline.java
@@ -3,9 +3,11 @@ package codesample.timeline;
 import java.util.*;
 
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
+/**
+ * This class implements an ordered collection that holds Event objects.
+ *
+ */
 public class Timeline implements Collection<Event> 
 {
 //	public static final DateTimeFormatter fmt = DateTimeFormat.forPattern("mm/dd/yyyy  kk:mm"); //"month/day/year  24hour:minutes of the hour"
@@ -16,6 +18,9 @@ public class Timeline implements Collection<Event>
 		return addToEventMap(eventMap, e);
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.util.Collection#addAll(java.util.Collection)
+	 */
 	public boolean addAll(Collection<? extends Event> c) {
 		
 		if (c !=null)
@@ -28,10 +33,16 @@ public class Timeline implements Collection<Event>
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.Collection#clear()
+	 */
 	public void clear() {
 		eventMap.clear(); // Clears the map
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.Collection#contains(java.lang.Object)
+	 */
 	public boolean contains(Object o) {
 		
 		if (o==null || !(o instanceof Event))
@@ -55,6 +66,9 @@ public class Timeline implements Collection<Event>
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.Collection#containsAll(java.util.Collection)
+	 */
 	public boolean containsAll(Collection<?> c) {
 		
 		if (c !=null)
@@ -72,6 +86,9 @@ public class Timeline implements Collection<Event>
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.Collection#isEmpty()
+	 */
 	public boolean isEmpty(){
 		if (eventMap.isEmpty()) // returns true if the map is empty
 		{
@@ -80,9 +97,199 @@ public class Timeline implements Collection<Event>
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.Collection#iterator()
+	 */
 	public Iterator<Event> iterator()
 	{
 		return new TimelineIterator();
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.util.Collection#remove(java.lang.Object)
+	 */
+	public boolean remove(Object o) {
+		if (o==null || !(o instanceof Event)) //if the object is null or not event return false, cannot remove
+		{
+			return false;
+		}
+		
+		Event myEvent = (Event)o;
+		DateTime startTime = myEvent.getStart();
+		
+		if (!eventMap.containsKey(startTime)) // if the eventMap does not contain the key return false, cannot remove
+		{
+			return false;
+		}
+		
+		if (eventMap.get(myEvent.getStart()).size()==1) // If the size of the set contains one event, remove the key
+		{
+			eventMap.remove(myEvent.getStart());
+			return true;
+		}
+				
+		HashSet<Event> events = eventMap.get(startTime); // else, remove the event located at a given key
+		return events.remove(myEvent);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Collection#removeAll(java.util.Collection)
+	 */
+	public boolean removeAll(Collection<?> c) {
+		if (c !=null)
+		{
+			Iterator<?> eventIter = c.iterator(); // iterate through the collection
+			while (eventIter.hasNext())           // remove all instances in eventMap from collection
+				this.remove(eventIter.next());
+			return true;
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Collection#retainAll(java.util.Collection)
+	 */
+	public boolean retainAll(Collection<?> c) {
+		
+		if(c!=null)
+		{
+			TreeMap<DateTime, HashSet<Event>> newEventMap = new TreeMap<DateTime, HashSet<Event>>();
+			
+			for (Object e: c)
+			{
+				if (e instanceof Event && this.contains(e))
+				{
+					addToEventMap(newEventMap, (Event)e);
+				}
+			}
+
+			if (sizeEventMap(eventMap) == sizeEventMap(newEventMap))
+			{
+				return false;
+			}
+			
+			eventMap= newEventMap;
+			return true;
+		}		
+		return false;	
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Collection#size()
+	 */
+	public int size() {	
+		return sizeEventMap(eventMap);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Collection#toArray()
+	 */
+	public Object[] toArray() {
+		if (!eventMap.isEmpty())
+		{
+			ArrayList<Event> list = new ArrayList<Event>();
+			for(DateTime key: eventMap.keySet())
+			{
+				for(Event e: eventMap.get(key))
+				{		
+					list.add(e);	
+				}
+			}	
+			return list.toArray();
+		}
+	
+		Object [] result = null;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Collection#toArray(java.lang.Object[])
+	 */
+	public <T> T[] toArray(T[] a) {
+		if (!eventMap.isEmpty())
+		{
+			ArrayList<Event> list = new ArrayList<Event>();
+			
+			for(DateTime key: eventMap.keySet())
+			{
+				for(Event e: eventMap.get(key))
+				{
+					for(int i=0; i<sizeEventMap(eventMap); i++)
+					{
+						System.out.println("[");
+						list.add(e);
+						System.out.println("]");
+					}
+				}
+			}	
+			return list.toArray(a);
+		}
+	
+		T [] result = null;
+		return result;
+	}
+	
+	
+	/**
+	 * Adds an Event to a specified map of DateTimes to sets of Events.
+	 * 
+	 * @param mapOfEvents The map of DateTimes to sets of Events to add the event to
+	 * @param e Event to add to the passed-in TreeMap of events
+	 * @return
+	 */
+	private boolean addToEventMap(TreeMap<DateTime, HashSet<Event>> mapOfEvents, Event e) {
+		if (e != null)
+		{
+			if(!mapOfEvents.containsKey(e.getStart()))   // If the DateTime is not there create a new key and hash set
+			{
+				mapOfEvents.put(e.getStart(), new HashSet<Event>()); // Places the DateTime and an empty Set into the map
+			}
+				mapOfEvents.get(e.getStart()).add(e); // Adds the new event to the empty Set in the given DateTime
+				return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Calculates the total size of a map of DateTimes to sets of Events
+	 * 
+	 * @param mapOfEvents The map of DateTimes to sets of Events to add the event to
+	 * @return 0 if empty, total number of events otherwise
+	 */
+	private int sizeEventMap(TreeMap<DateTime, HashSet<Event>> mapOfEvents) {
+		
+		int count =0; // Count variable to keep track of all the values in eventMap
+
+		if (!mapOfEvents.isEmpty())
+		{
+			for(HashSet<Event> set: mapOfEvents.values())
+			{
+				count += set.size();
+			}
+				return count;
+		}
+		return 0;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		for (DateTime key: eventMap.keySet()) //the keys.
+		{
+			sb.append("[<").append(key).append(">: ");
+			for (Event e: eventMap.get(key)) //gets each individual event  
+			{
+				sb.append("[").append(e).append("]");
+				
+			}
+			sb.append("\n");
+		}
+		
+		
+		return sb.toString();
 	}
 	
 	/**
@@ -90,8 +297,8 @@ public class Timeline implements Collection<Event>
      * asymmetrical 2-dimensional array. To do this, we're only keeping track of
      * indexKey and indexValue and doing the navigation through the underlying
      * eventMap to determine what will actually work.
-     *
      * 
+     * Does not implement the optional add, remove, or set operations
      *
      */
     private class TimelineIterator implements ListIterator<Event>
@@ -106,6 +313,9 @@ public class Timeline implements Collection<Event>
         
         Object[] eventSets = new Object[0];
         
+        /**
+         *  No-argument protected constructor.
+         */
         protected TimelineIterator()
         {
             if (eventMap != null && !eventMap.isEmpty())
@@ -119,6 +329,9 @@ public class Timeline implements Collection<Event>
         }
         
 
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#hasNext()
+         */
         public boolean hasNext() 
         {
         	
@@ -149,6 +362,9 @@ public class Timeline implements Collection<Event>
         }
 
         
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#next()
+         */
         public Event next()
         {
         	eventSet = (HashSet<Event>)eventSets[eventSetIndex];
@@ -187,6 +403,9 @@ public class Timeline implements Collection<Event>
             return nextEvent;
         }
 
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#hasPrevious()
+         */
         public boolean hasPrevious()
         {
         	
@@ -209,6 +428,9 @@ public class Timeline implements Collection<Event>
             }
         }
 
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#previous()
+         */
         public Event previous() 
         {
 			eventSet = (HashSet<Event>)eventSets[eventSetIndex];
@@ -246,190 +468,47 @@ public class Timeline implements Collection<Event>
         }
         
 
-       public int nextIndex() //of the all the events or just the events in a particular set.
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#nextIndex()
+	     */
+        public int nextIndex() //of the all the events or just the events in a particular set.
 		{
         	return eventsPosition;
         }
 
 		
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#previousIndex()
+         */
         public int previousIndex() {
             return eventsPosition-1; // Since eventsPosition is declared at 1 subtract one when going backwards
         }
 
+        /* (non-Javadoc)
+         * @see java.util.ListIterator#remove()
+         */
         public void remove()
         {
             throw new UnsupportedOperationException(
                     "We're not implementing remove!");
         }
 
-        // not a necessary method for our case.
+        /** Not implemented by Timeline
+         *
+         */
         public void set(Event e)
         {
             throw new UnsupportedOperationException(
                     "We're not implementing set!");
         }
 
-        public void add(Event e)
+        /** Not implemented by Timeline
+        *
+        */
+       public void add(Event e)
         {
         	throw new UnsupportedOperationException(
                     "We're not implementing add!");
         }
-}
-
-
-	public boolean remove(Object o) {
-		if (o==null || !(o instanceof Event)) //if the object is null or not event return false, cannot remove
-		{
-			return false;
-		}
-		
-		Event myEvent = (Event)o;
-		DateTime startTime = myEvent.getStart();
-		
-		if (!eventMap.containsKey(startTime)) // if the eventMap does not contain the key return false, cannot remove
-		{
-			return false;
-		}
-		
-		if (eventMap.get(myEvent.getStart()).size()==1) // If the size of the set contains one event, remove the key
-		{
-			eventMap.remove(myEvent.getStart());
-			return true;
-		}
-				
-		HashSet<Event> events = eventMap.get(startTime); // else, remove the event located at a given key
-		return events.remove(myEvent);
-	}
-
-	public boolean removeAll(Collection<?> c) {
-		if (c !=null)
-		{
-			Iterator<?> eventIter = c.iterator(); // iterate through the collection
-			while (eventIter.hasNext())           // remove all instances in eventMap from collection
-				this.remove(eventIter.next());
-			return true;
-		}
-		return false;
-	}
-
-	public boolean retainAll(Collection<?> c) {
-		
-		if(c!=null)
-		{
-			TreeMap<DateTime, HashSet<Event>> newEventMap = new TreeMap<DateTime, HashSet<Event>>();
-			
-			for (Object e: c)
-			{
-				if (e instanceof Event && this.contains(e))
-				{
-					addToEventMap(newEventMap, (Event)e);
-				}
-			}
-
-			if (sizeEventMap(eventMap) == sizeEventMap(newEventMap))
-			{
-				return false;
-			}
-			
-			eventMap= newEventMap;
-			return true;
-		}		
-	return false;
-		
-	}
-
-	public int size() {	
-		return sizeEventMap(eventMap);
-	}
-
-	public Object[] toArray() {
-		if (!eventMap.isEmpty())
-		{
-//			Object [] list = new Object[sizeEventMap(eventMap)];
-			ArrayList<Event> list = new ArrayList<Event>();
-			for(DateTime key: eventMap.keySet())
-			{
-				for(Event e: eventMap.get(key))
-				{		
-					list.add(e);	
-				}
-			}	
-			return list.toArray();
-		}
-	
-		Object [] result = null;
-		return result;
-	}
-
-	public <T> T[] toArray(T[] a) {
-		if (!eventMap.isEmpty())
-		{
-			ArrayList<Event> list = new ArrayList<Event>();
-			
-			for(DateTime key: eventMap.keySet())
-			{
-				for(Event e: eventMap.get(key))
-				{
-					for(int i=0; i<sizeEventMap(eventMap); i++)
-					{
-						System.out.println("[");
-						list.add(e);
-						System.out.println("]");
-					}
-				}
-			}	
-			return list.toArray(a);
-		}
-	
-		T [] result = null;
-		return result;
-	}
-	
-	
-	// Private add method for any TreeMap (Used in retainAll)
-	private boolean addToEventMap(TreeMap<DateTime, HashSet<Event>> set, Event e) {
-		if (e != null)
-		{
-			if(!set.containsKey(e.getStart()))   // If the DateTime is not there create a new key and hash set
-			{
-				set.put(e.getStart(), new HashSet<Event>()); // Places the DateTime and an empty Set into the map
-			}
-				set.get(e.getStart()).add(e); // Adds the new event to the empty Set in the given DateTime
-				return true;
-		}
-			return false;
-	}
-	
-	// Private size method for any TreeMap (Used in retainAll)
-	private int sizeEventMap(TreeMap<DateTime, HashSet<Event>> map) {
-		
-		int count =0; // Count variable to keep track of all the values in eventMap
-
-		if (!map.isEmpty())
-		{
-			for(HashSet<Event> set: map.values())
-			{
-				count += set.size();
-			}
-				return count;
-		}
-		return 0; // If it is empty return size of 0
-	}
-	public String toString() //For Printing out the timeline
-	{
-		StringBuilder sb = new StringBuilder();
-		for (DateTime key: eventMap.keySet()) //the keys.
-		{
-			sb.append("[<").append(key).append(">: ");
-			for (Event e: eventMap.get(key)) //gets each individual event  
-			{
-				sb.append("[").append(e).append("]");
-				
-			}
-			sb.append("\n");
-		}
-		
-		
-		return sb.toString();
-	}
+    }
 }
