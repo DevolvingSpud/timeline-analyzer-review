f8a4d0d4c83f0fef88931db29a317ca8009caee8
shyguy <suppusmac@gmail.com>
2013-07-29 15:35:59 -0400

    Added methods to EmailEvent and some of their test cases.

--- timeline/src/main/java/codesample/timeline/EmailEvent.java
+++ timeline/src/main/java/codesample/timeline/EmailEvent.java
@@ -1,84 +1,67 @@
 package codesample.timeline;
-
-import java.sql.Array;
 import java.util.Date;
 
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 import org.joda.time.DateTime;
 
-import codesample.timeline.AbstractEvent;
 
 public class EmailEvent implements Event
 {
-	DateTime startDate = null;
+	DateTime startDate = null; //the sent time
+	//DateTime endDate = null; //the received time
 	private MimeMessage message;
 	
-	/**
-	 * EmailEvent requires a sent date, which is pulled from a MimeMessage of 
-	 * an E-mail. 
+	/** EmailEvent requires a sent date, which is pulled from a MimeMessage of an E-mail. 
 	 * @param message - a MimeMessage containing the content of an E-mail.
 	 * @throws MessagingException
-	 */
+	 * The startDate represents the time sent.
+	 * The endDate represents the time received*/
 	public EmailEvent(MimeMessage message) throws MessagingException
 	{
 		this.message = message;
 		startDate = new DateTime(message.getSentDate());
 	}
 	
-	/**
-	 * Receives MessageID header in E-mail.
-	 * @return MessageID header.
-	 * @throws MessagingException
-	 */
-	public String getMessageID() throws MessagingException
-	{
-		return message.getMessageID();
-	}
 	
-	/**
-	 * @return sent date of an E-mail.
-	 */
+	/** @return sent date of E-mail.*/
 	public DateTime getStart() 
 	{
-		return startDate;
+		return  startDate;
 	}
 	
-	/**
-	 * @return Java.util Date instead of DateTime of an E-mails Received Time.
-	 * @throws MessagingException
-	 */
-	//Not completely sure if this Received date is necessary yet...question on board in red.
-	public Date getStartOfRecieved() throws MessagingException
-	{
-		return message.getReceivedDate();
-	}
 	
-	/**
-	 * Returns a the sent date of an E-mail b/c E-mails do not contain end times.
-	 * @return SentDate of an E-mail.
-	 */
+	/** @return sent date of E-mail. */
 	public DateTime getEnd() 
 	{
 		return startDate;
 	}
 	
+	
 	/**
-	 * @return An array of addresses belonging to the Sender(s).
-	 * @throws MessagingException 
-	 */
+	 * Receives MessageID header in E-mail hopefully to give
+	 * this message a unique ID number.
+	 * @return MessageID header.
+	 * @throws MessagingException
+	 *//*
+	public String getMessageID() throws MessagingException 
+	{
+		return message.getMessageID();
+	}*/
+	
+	
+	/** @return An array of addresses belonging to the Sender(s).
+	 * @throws MessagingException */
 	public Address[] getSenderEmail() throws MessagingException
 	{
 		return message.getFrom(); 
 	}
 	
-	/**
-	 * @return the primary recipient's E-mail address/addresses.
-	 * @throws MessagingException
-	 */
+	
+	/** @return the primary recipient's E-mail address/addresses.
+	 * @throws MessagingException */
 	public Address[] getReceiverEmail() throws MessagingException
 	{
 		return message.getRecipients(Message.RecipientType.TO); 
@@ -87,51 +70,60 @@ public class EmailEvent implements Event
 		 */
 	}
 	
-	//return if both the sender and receiver both have company E-mails.
-	public boolean isBothFromCompany()
+	/*//not necessary yet.
+	*//** @return if both the sender and receiver both have company E-mails.*//*
+	public boolean isBothFromCo()
 	{
+		if getSenderEmail().equals(get.ReceiverEmail()
+			return true;
 		return false;
-	}
+	}*/
 	
-	/**
-	 * @return the subject header of an E-mail.
-	 * @throws MessagingException
-	 */
+	
+	/** @return the subject header of an E-mail.
+	 * @throws MessagingException*/
 	public String getSubject() throws MessagingException
 	{
 		return message.getSubject();
 	}
 	
-	//returns sender's name ==> there is not a method for this in Mime.Message API.
+	
+	/**@return sender's name ==> there is not a method for this in Mime.Message API. */
 	public String getSenderName()
 	{
 		return null;
 	}
 	
-	/*return sender's name ==> before attempting this method make sure 
+	
+	/**@return sender's name ==> before attempting this method make sure 
 	 * that getReceiverEmail() works (look at comment in getReceiver to understand).
 	 */
-	//return receiver's name
 	public String getReceiverName()
 	{
 		return null;
 	}
 	
-	/**
-	 * @return array of carbon-copy (Cc) recipient's E-mail addresses.
-	 * @throws MessagingException 
-	 */
+	
+	/** @return array of carbon-copy (Cc) recipient's E-mail addresses.
+	 * @throws MessagingException */
 	public Address[] getCC() throws MessagingException
 	{
 		return message.getRecipients(Message.RecipientType.CC); 
 	}
 	
-	/**
-	 * @return array of blind carbon-copy (Bcc) recipient's E-mail addresses.
-	 * @throws MessagingException 
-	 */
+	
+	/** @return array of blind carbon-copy (Bcc) recipient's E-mail addresses.
+	 * @throws MessagingException */
 	public Address[] getBCC() throws MessagingException
 	{
 		return message.getRecipients(Message.RecipientType.TO); 
+	}
+	
+	
+	/** @return size of E-mail/content in bytes. 
+	 * @throws MessagingException */
+	public int getSize() throws MessagingException
+	{
+		return message.getSize();
 	}
 }
--- timeline/src/test/java/codesample/timeline/TestEmailEvents.java
+++ timeline/src/test/java/codesample/timeline/TestEmailEvents.java
@@ -1,32 +1,166 @@
 package codesample.timeline;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.util.Date;
 import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class TestEmailEvents 
 {
-	
-	//doesn't work yet...
 	@Test
 	public void TestGetStart() throws MessagingException
 	{
-		DateTime d = new DateTime(94,11,25,0,0);
+		DateTime d = new DateTime(1994,11,25,0,0); //time set
+		Date e = d.toDate(); //convert to Java.util date format
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
 		
+		MimeMessage mime = new MimeMessage(session); 
+		mime.setSentDate(e);  //setting date.
+		EmailEvent event = new EmailEvent(mime); //make an event for extracting
+		//System.out.println(event.getStart().toDate());
+		//System.out.println(e);
+
+		assertEquals(e,event.getStart()); //testing the event
+	}
+	
+	@Test
+	public void TestGetEnd() throws MessagingException 
+	{
+		DateTime d = new DateTime(94,11,25,0,0); //time set.
+		Date e = d.toDate(); //convert to java.util.Date.
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
+		mime.setSentDate(e); //setting the date.
+		EmailEvent event = new EmailEvent(mime);
+		
+		assertEquals(e, event.getStart());
+	}
+	
+	
+	//WEB ADDRESS: https://forums.oracle.com/thread/2178060
+	/*//not sure how to test this...I clearly have to set a message ID.
+	@Test
+	public void TestGetMessageID() throws MessagingException
+	{
+		String id = "22287574.1075863594524"; //message ID
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
+		mime.set
+		EmailEvent event = new EmailEvent(mime);
+		assertEquals(id, event.getMessageID());
+	}*/
+	
+	@Test
+	public void TestGetSenderEmail()
+	{
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 		MimeMessage mime = new MimeMessage(session);
 		
-		mime.setSentDate(d.toDate()); 
+		Address [] senders = new Address[] {InternetAddress.parse("example1@email.com"),
+										InternetAddress.parse("example2@email.com"),
+										InternetAddress.parse("example3@email.com")};
+		
+		mime.setSender(senders);
 		EmailEvent event = new EmailEvent(mime);
+		assertEquals(senders, event.getSenderEmail());
+	}
+	
+	@Test
+	public void TestGetReceiverEmail()
+	{
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
 		
-		DateTime dt = new DateTime(d);
+		Address [] receivers = new Address[] {InternetAddress.parse("example1@email.com"),
+										InternetAddress.parse("example2@email.com"),
+										InternetAddress.parse("example3@email.com")};
 		
-		assertEquals(event.getStart(), dt);
+		mime.setRecipient(Message.RecipientType.TO, receivers);
+		EmailEvent event = new EmailEvent(mime);
+		assertEquals(receivers, event.getCC());
+	}
+	
+	@Test
+	public void TestIsBothFromCo()
+	{
+		fail();
+	}
+	
+	@Test 
+	public void TestGetSubject() throws MessagingException
+	{
+		String s = "Blank Subject";
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
+		mime.setSubject(s);
+		EmailEvent event = new EmailEvent(mime);
+		assertEquals(s, event.getSubject());
+		
+	}
+	
+	@Test
+	public void TestGetSenderName()
+	{
+		fail();
+	}
+	
+	@Test
+	public void TestGetReceiverName()
+	{
+		fail();
+	}
+	
+	@Test 
+	public void TestGetCC()
+	{
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
+		
+		Address [] cc = new Address[] {InternetAddress.parse("example1@email.com"),
+										InternetAddress.parse("example2@email.com"),
+										InternetAddress.parse("example3@email.com")};
+		
+		mime.setRecipient(Message.RecipientType.CC, cc);
+		EmailEvent event = new EmailEvent(mime);
+		assertEquals(s, event.getCC());
+	}
+	
+	@Test
+	public void TestBCC()
+	{
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage mime = new MimeMessage(session);
+		
+		Address [] bcc = new Address[] {InternetAddress.parse("example1@email.com"),
+										InternetAddress.parse("example2@email.com"),
+										InternetAddress.parse("example3@email.com")};
+		
+		mime.setRecipient(Message.RecipientType.BCC, bcc);
+		EmailEvent event = new EmailEvent(mime);
+		assertEquals(bcc, event.getCC());
+	}
+	
+	@Test
+	public void TestSize()
+	{
+		
 	}
 }
